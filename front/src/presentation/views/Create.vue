435b247414105e5e79e222131475be406ff6f57d
Felipe Barreiros <felipewww@outlook.com.br>
2023-04-06 17:33:50 -0300

    create css

--- front/src/presentation/views/Create.vue
+++ front/src/presentation/views/Create.vue
@@ -1,34 +1,37 @@
 <template>
-    <div>
+    <div class="list-container">
         <form>
-            <div>
+            <div class="ipt-box">
                 <label>Name</label>
                 <input type="text" v-model="knight.name">
             </div>
 
-            <div>
+            <div class="ipt-box">
                 <label>Nickname</label>
                 <input type="text" v-model="knight.nickname">
             </div>
 
-            <div>
+            <div class="ipt-box">
                 <label>Birthday</label>
                 <input type="text" v-model="knight.birthday">
             </div>
 
-            <select v-model="knight.keyAttribute">
-                <option v-for="attr in EKnightKeyAttributes">{{attr}}</option>
-            </select>
-
-            <div v-for="attr in EKnightKeyAttributes">
-                <span>{{ attr }}</span>
+            <div class="ipt-box" v-for="attr in EKnightKeyAttributes">
+                <label>{{ attr }}</label>
                 <input type="number" min="0" v-model="knight.attributes[attr]">
+            </div>
+
+            <div class="ipt-box">
+                <label>Key Attribute</label>
+                <select v-model="knight.keyAttribute">
+                    <option v-for="attr in EKnightKeyAttributes">{{attr}}</option>
+                </select>
             </div>
         </form>
 
-        <div>{{knight}}</div>
+        <br>
 
-        <div @click="repo.create(knight)">save</div>
+        <div class="btn-gold" @click="repo.create(knight)">save</div>
     </div>
 </template>
 
@@ -60,5 +63,22 @@ export default defineComponent({
 </script>
 
 <style scoped>
+label {
+    width: 115px;
+    display: inline-block;
+}
 
+.ipt-box input {
+    font-size: 20px;
+    background: #000;
+    border: 1px solid gold;
+    color: #fff;
+    padding: 5px;
+    border-radius: 3px;
+}
+
+.ipt-box {
+    padding: 9px 0;
+    font-size: 18px;
+}
 </style>
